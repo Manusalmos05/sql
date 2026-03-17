573df00e967d3100e3f8576111ec2c8aefbece55
Manuelasalmos05 <manuelasalazarmoscoso@gmail.com>
2026-03-17 11:46:33 +0100

    update

--- datacontrl/tarea5/consultas.sql
+++ datacontrl/tarea5/consultas.sql
@@ -3,3 +3,21 @@ SELECT j.id, j.nombre, j.apellido
 FROM jugador as j
 LEFT JOIN jugadorposicion as jp ON j.id=jp.jugadorid
 WHERE jp.posicionid is null; 
+
+
+/*Equipo con mas jugadores*/
+SELECT e.ID, e.nombre, COUNT(DISTINCT j.id) as NumeroJugadores
+FROM jugador as j
+JOIN equipo as e ON j.equipoID= e.ID
+GROUP BY e.nombre, e.ID
+ORDER BY NumeroJugadores DESC
+LIMIT 1;
+
+/*jugadores por cada liga*/
+SELECT l.ID, l.Nombre, COUNT(j.ID) as NumeroJugadores
+FROM Liga as l
+LEFT JOIN Equipo as e ON l.ID = e.LigaID
+LEFT JOIN Jugador as j ON e.ID = j.EquipoID
+GROUP BY l.ID, l.Nombre
+ORDER BY NumeroJugadores DESC;
+
