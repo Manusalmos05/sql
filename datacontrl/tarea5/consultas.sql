91c61624ae566e62102fc1e0c63879e12f4df8fe
Manuelasalmos05 <manuelasalazarmoscoso@gmail.com>
2026-03-18 15:31:42 +0100

    final

--- datacontrl/tarea5/consultas.sql
+++ datacontrl/tarea5/consultas.sql
@@ -60,3 +60,69 @@ WHERE l.pais <> (
         ELSE e.Nacionalidad 
     END);
 
+/*jugadores con la misma nacionalidad que su entrenador*/
+
+SELECT j.nombre as NombreJugador, j.nacionalidad as NacionalidadJugador,
+e.nombre as NombreEntrenador, e.nacionalidad as NacionalidadEntrenador
+
+FROM jugador as j
+JOIN equipo as eq ON j.EquipoID=eq.ID
+JOIN entrenador as e ON  eq.ID=e.EquipoID
+WHERE j.nacionalidad = e.nacionalidad;
+
+
+/*posicion mas asignada a jugadores*/
+
+SELECT p.nombre, COUNT(jp.JugadorID) as NumeroJugadores
+FROM jugadorposicion as jp 
+JOIN posicion as p ON jp.posicionID =p.ID
+GROUP BY p.nombre, p.id
+ORDER BY COUNT(jp.JugadorID) DESC
+LIMIT 1;
+
+/*posicion menos asignada a jugadores*/
+
+SELECT p.nombre, COUNT(jp.JugadorID) as NumeroJugadores
+FROM jugadorposicion as jp 
+JOIN posicion as p ON jp.posicionID =p.ID
+GROUP BY p.nombre, p.id
+ORDER BY COUNT(jp.JugadorID) ASC
+LIMIT 1;
+
+
+/*Entrenadores cuya nacionalidad no coincide con la de los jugadores de su equipo*/
+
+SELECT e.Nombre, e.Nacionalidad, eq.Nombre AS Equipo
+FROM Entrenador as e
+JOIN Equipo as eq ON e.EquipoID = eq.ID
+LEFT JOIN Jugador as j ON e.EquipoID = j.EquipoID AND e.Nacionalidad = j.Nacionalidad /*que no haya jugadores con esa nacionalidad en su equipo*/
+WHERE j.ID IS NULL;
+
+/*jugadores con posicion unica dentro de su equipo*/
+
+SELECT j.id, j.Nombre, p.nombre as Posicion, eq.Nombre AS Equipo
+FROM jugador as j
+JOIN Equipo eq ON j.EquipoID = eq.ID
+JOIN jugadorposicion as jp ON j.ID =jp.JugadorID
+JOIN posicion as p ON jp.PosicionID=p.ID
+WHERE (/*aqui compara cada jugador dentro de su equipo*/
+    SELECT COUNT(*) 
+    FROM JugadorPosicion jp2
+    JOIN Jugador j2 ON jp2.JugadorID = j2.ID
+    WHERE j2.EquipoID = j.EquipoID 
+    AND jp2.PosicionID = jp.PosicionID
+) = 1;
+
+/*tipo de jugador segun sus posiciones*/
+SELECT j.id, j.Nombre, eq.Nombre AS Equipo,
+(SELECT COUNT(*) 
+     FROM JugadorPosicion jp 
+     WHERE jp.JugadorID = j.ID) AS NumeroPosiciones,
+    CASE 
+        WHEN (SELECT COUNT(*) FROM JugadorPosicion jp WHERE jp.JugadorID = j.ID) > 1 THEN 'Versátil'
+        WHEN (SELECT COUNT(*) FROM JugadorPosicion jp WHERE jp.JugadorID = j.ID) =0 THEN 'Sin Posicion'
+        ELSE 'Especialista'
+    END AS Tipo
+   
+FROM Jugador j
+JOIN Equipo eq ON j.EquipoID = eq.ID;
